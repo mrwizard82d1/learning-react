aacc0496b70b5b8fa69a52e12020d779fead1c44
Larry Jones <larry.jones@reveal-energy.com>
2022-08-04 04:16:59 -0500

    Use hard-coded recipe list for App

--- recipes-app/src/App.tsx
+++ recipes-app/src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>1 lb Salmon</li>
+        <li>2 cups Pine Nuts</li>
+        <li>2 cups Butter Lettuce</li>
+        <li>1 Yellow Squash</li>
+        <li>1/2 cup Olive Oil</li>
+        <li>3 Cloves of Garlic</li>
+      </ul>
     </div>
   );
 }
