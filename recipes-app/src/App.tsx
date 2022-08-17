d086efcc3b01849e9e8af216c7bf06736da538fd
laj <larry.jones@reveal-energy.com>
2022-08-17 10:18:34 -0500

    Reduce duplicated code

--- recipes-app/src/App.tsx
+++ recipes-app/src/App.tsx
@@ -1,55 +1,35 @@
 import React from 'react';
 import data from './data';
+import { A } from '@mobily/ts-belt';
 import './App.css';
+
+function Ingredient({ name, amount, measurement }: {name: string, amount: number, measurement: string}) {
+    return <li>{`${amount} ${measurement} ${name}`}</li>
+}
+
+function Instruction(instruction: string) {
+    return <li>{instruction}</li>
+}
 
 function App() {
     let salmon = data[0];
     let fishTacos = data[1];
-
-    const presentIngredient = (ingredient: {name: string, amount: number, measurement: string}) => {
-        let { name, amount, measurement } = ingredient;
-        return `${amount} ${measurement} ${name}`
-    };
 
     return (
         <div className="App">
             <div>
                 <h1>{salmon.name}</h1>
                 <h2>Ingredients</h2>
-                    <ul>
-                        <li>{presentIngredient(salmon.ingredients[0])}</li>
-                        <li>{presentIngredient(salmon.ingredients[1])}</li>
-                        <li>{presentIngredient(salmon.ingredients[2])}</li>
-                        <li>{presentIngredient(salmon.ingredients[3])}</li>
-                        <li>{presentIngredient(salmon.ingredients[4])}</li>
-                        <li>{presentIngredient(salmon.ingredients[5])}</li>
-                    </ul>
+                    <ul> { A.map(salmon.ingredients, Ingredient)} </ul>
                 <h2>Steps</h2>
-                    <ul>
-                        <li>{salmon.steps[0]}</li>
-                        <li>{salmon.steps[1]}</li>
-                        <li>{salmon.steps[2]}</li>
-                        <li>{salmon.steps[3]}</li>
-                        <li>{salmon.steps[4]}</li>
-                        <li>{salmon.steps[5]}</li>
-                    </ul>
+                    <ul> { A.map(salmon.steps, Instruction) } </ul>
             </div>
             <div>
                 <h1>{fishTacos.name}</h1>
                 <h2>Ingredients</h2>
-                <ul>
-                    <li>{presentIngredient(fishTacos.ingredients[0])}</li>
-                    <li>{presentIngredient(fishTacos.ingredients[1])}</li>
-                    <li>{presentIngredient(fishTacos.ingredients[2])}</li>
-                    <li>{presentIngredient(fishTacos.ingredients[3])}</li>
-                    <li>{presentIngredient(fishTacos.ingredients[4])}</li>
-                </ul>
+                <ul> { A.map(fishTacos.ingredients, Ingredient)} </ul>
                 <h2>Steps</h2>
-                <ul>
-                    <li>{fishTacos.steps[0]}</li>
-                    <li>{fishTacos.steps[1]}</li>
-                    <li>{fishTacos.steps[2]}</li>
-                </ul>
+                <ul> { A.map(fishTacos.steps, Instruction) } </ul>
             </div>
         </div>
     );
